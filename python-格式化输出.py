351cc5db074f2b62d56b41a47e959991cfd9a1b2
buffetgod <129254572+buffetgod@users.noreply.github.com>
2024-07-24 09:31:46 +0800

    Create python-格式化输出.py

---
+++ python-格式化输出.py
@@ -0,0 +1,12 @@
+a='xujianwei'
+b=12.123456789
+c=9
+##  %输出
+print('my name is %s and my age is %d'%(a,b)) 
+print('%.1f'%b)
+print('%o'%c)
+##  .format输出
+print('{},{}'.format(a,b))
+print('{},{}'.format(b,a))
+##  f'{}'输出
+print(f'{a},{b}')
