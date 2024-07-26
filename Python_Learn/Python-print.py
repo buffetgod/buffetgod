fa82b354acbb5e432ffe2c1afac094471355fde4
buffetgod <129254572+buffetgod@users.noreply.github.com>
2024-07-26 11:22:00 +0800

    Update Python-print.py

--- Python_Learn/Python-print.py
+++ Python_Learn/Python-print.py
@@ -10,3 +10,16 @@ print('{},{}'.format(a,b))
 print('{},{}'.format(b,a))
 ##  f'{}'输出
 print(f'{a},{b}')
+## 转义字符
+print('we\'re friend')
+print('we\\er')
+print('hellow\tworld')
+print('\"hellow world\"')
+##运算符
+print(10+10)
+print(10-5)
+print(10*2)
+print(10/2)
+print(10%2)  #取余数
+print(10//3) #整除
+print(2**3)  #次方
